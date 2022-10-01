0219db265a06686c249793523eef80d0abd664a7
Marvin Hozi <marvinhozi@yahoo.com>
2022-10-01 14:05:46 -0700

    Added character counting and position checking. Fixed char counting bugs.

--- mastermind.asm
+++ mastermind.asm
@@ -4,6 +4,7 @@
 	welcome: .asciiz "Welcome to Mastermind\n"	# welcome message
 	guessPrompt: .asciiz "Guess a 4-letter string using only A-E: "
 	correctLetters: .asciiz "Correct Letters: "
+	correctPositions: .asciiz "\nCorrect Positions: "
 	open: .asciiz "("
 	close: .asciiz ")\n\n"
 
@@ -91,6 +92,7 @@ outerLoop:
 	jal readInput # call function to read user input
 	jal backupArraysToStack
 	addi $s2, $s2, 1 # total tries counter, we need this to stick around
+	li $t6, 48 # ASCII "0" to remove matching chars!
 	
 outerNumCharsCorrectLoop:
 	beq $t1, 4, outerNumCharsCorrectLoopEnd
@@ -116,53 +118,77 @@ innerLoopEnd:
 outerNumCharsCorrectLoopEnd:
 la $s0, randWordArray
 la $s1, userInput
+move $s3, $t5
+li $t1, 0
+li $t2, 0
+li $t3, 0
+li $t4, 0
+li $t5, 0
+li $t6, 0
 jal restoreArraysFromStack
+j checkCorrectCharsLoop
+
 # Good progress! Just zero out the registers used previously, restore the values of the arrays,
 # free up the used stack space, prepare for the number position checking loop!
 	
-	
 zeroBytesAndIncrement:
-	sb, $zero, 0($s0)
-	sb, $zero, 0($s1)
+	addi $t6, $t6, 1
+	sb $t6, 0($s0)
+	addiu $t6, $t6, 1
+	sb $t6, 0($s1)
 	addi $t5, $t5, 1
 	j continueInnerLoop
 	
 checkCorrectCharsLoop:
 	lbu $t1, 0($s0) # load the first character from the random words array
 	lbu $t2, 0($s1) # load the first character from the user input array
-	beq $t1, $t2, charsMatchIncrement # if the characters match, increment the counter
-	
+	bne $t1, $t2, continueCharCorrectLoop # if the characters match, increment the counter
+	addi $t4, $t4, 1 # increment by 1
+
 continueCharCorrectLoop: # continue the loop after we increment the counter
 	addi $s0, $s0, 1 # go to the next character in the rand char array
 	addi $s1, $s1, 1 # go to the next character in the user input array
 	addi $t3, $t3, 1 # loop counter
 	bne $t3, 4, checkCorrectCharsLoop # restart the loop until we iterate 4 times (4 chars)
-	li $t1, 0 # reset the register
-	li $t2, 0 # reset the register
+	move $s4, $t4
+	li $t1, 0
+	li $t2, 0
+	li $t3, 0
+	li $t4, 0
 	la $s0, randWordArray # reset the address
 	la $s1, userInput # reset the address
 	
-	j outputResultDEBUGGING # FOR DEBUGGING ONLY, JUST WANT TO SEE CORRECT CHAR AMOUNTS
-	
-charsMatchIncrement: # used to increment the number of correct chars counter
-	addi $t4, $t4, 1 # increment by 1
-	j continueCharCorrectLoop # continue the character iteration loop
-	
-outputResultDEBUGGING:	# FOR DEBUGGING ONLY, JUST WANT TO SEE CORRECT CHAR AMOUNTS
+outputResults:	# FOR DEBUGGING ONLY, JUST WANT TO SEE CORRECT CHAR AMOUNTS
 	la $a0, correctLetters
 	li $v0, 4
 	syscall
 	
-	move $a0, $t4
+	move $a0, $s3
 	li $v0, 1
 	syscall
+	
+	la $a0, correctPositions
+	li $v0, 4
+	syscall
+	
+	move $a0, $s4
+	li $v0, 1
+	syscall
+	
+	li $a0, 10
+	li $v0, 11
+	syscall
+	
+	
+	slti $s5, $s3, 4
+	slti $s5, $s4, 4
+	lw $ra, 0($sp) # load main's next instruction
+	bne $s5, 0, outerLoop
 	
 	
 
 	# now we need to work on the logic for what happens if the user guess is wrong
 	# we also need to track number of correct character positions
-	
-	lw $ra, 0($sp) # load main's next instruction
 	jr $ra # go to main!
 
 exit:
